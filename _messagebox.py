b3729c5296f35718b5211f4e815eeb30cd062641
Kewth <wthdsg@foxmail.com>
2019-01-17 19:02:04 +0800

    add --ellipsize to zenity

--- _messagebox.py
+++ _messagebox.py
@@ -3,15 +3,15 @@ import os
 
 def info(mes):
     'print [mes] on a new window'
-    os.system('zenity 2> /dev/null --info \
+    os.system('zenity 2> /dev/null --info --ellipsize\
             --title=\'pychat\' --text=\'' + mes + '\'')
 
 def warning(mes):
     'print [mes] on a new windows as a warning'
-    os.system('zenity 2> /dev/null --warning \
+    os.system('zenity 2> /dev/null --warning --ellipsize\
             --title=\'warning from pychat\' --text=\'' + mes + '\'')
 
 def error(mes):
     'print [mes] on a new windows as an error'
-    os.system('zenity 2> /dev/null --error \
+    os.system('zenity 2> /dev/null --error --ellipsize\
             --title=\'warning from pychat\' --text=\'' + mes + '\'')
